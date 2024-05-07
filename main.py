39a3a3e608cd3bd080787490e51a7d64092fdd0a
Cristian Alzate <calzateu@eafit.edu.co>
2024-05-07 13:25:25 -0500

    feat: added optimization.

--- main.py
+++ main.py
@@ -1,4 +1,84 @@
+import optimization as go
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
 
 
 if __name__ == "__main__":
-    pass
+    path = os.getcwd()
+    data = pd.read_excel(path + '/data/1_n_128_dis_c_3_ln_8_seed_23032023_node_data.xlsx')
+    points = data[data['Type'] == 'Vd'][['lon', 'lat']]
+
+    plt.plot(points['lon'], points['lat'], 'o', color='b')
+    plt.show()
+
+    K_clusters = range(1, 10)
+    kmeans_models = [KMeans(n_clusters=i) for i in K_clusters]
+    score = [kmeans_models[i].fit(points).score(points) for i in range(len(kmeans_models))]
+    # Plotting
+    plt.plot(K_clusters, score)
+    plt.xlabel('Number of Clusters')
+    plt.ylabel('Score')
+    plt.title('Elbow Curve')
+    plt.show()
+
+    kmeans = KMeans(n_clusters=4, init='k-means++')
+    kmeans.fit(points[points.columns[0:2]])  # Compute k-means clustering.
+    points['cluster_label'] = kmeans.labels_
+
+    centers = kmeans.cluster_centers_  # Coordinates of cluster centers.
+    labels = points['cluster_label']  # Labels of each point
+
+    points.plot.scatter(x='lon', y='lat', c=labels, s=50, cmap='viridis')
+    plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+    plt.show()
+
+    # Build matrix to tell if the point i is in cluster j
+    membership_matrix = np.zeros((len(points), len(centers)))
+
+    for i in range(len(points)):
+        for j in range(len(centers)):
+            if points['cluster_label'][i] == j:
+                membership_matrix[i][j] = 1
+            else:
+                membership_matrix[i][j] = 0
+
+    print(membership_matrix)
+
+    Q_c = np.array([32, 32, 32, 32])
+    q = np.array([1 for i in range(len(points))])
+
+    model_pulp = go.relocate_pulp(membership_matrix, Q_c, q, points)
+
+    variables = model_pulp.variablesDict()
+
+    # Copy point into points_pulp
+    points_pulp = points.copy()
+    for i in range(len(points_pulp)):
+        for j in range(len(centers)):
+            if variables['x_ic_add__(%d,_%d)' % (i, j)].value() == 1:
+                points_pulp.loc[i, 'cluster_label'] = j
+                print(i, j)
+
+    model = go.relocate(membership_matrix, Q_c, q, points)
+
+    points_pulp.plot.scatter(x='lon', y='lat', c=labels, s=50, cmap='viridis')
+    plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+    plt.show()
+
+    # variables = model.getVars()
+    # points_gurobi = points.copy()
+    # for var in variables:
+    #     # if var.X == 1:
+    #     if var.VarName.startswith("x_ic_add__") and var.X == 1:
+    #         node, cluster = var.VarName.split('_')[-1].split(',')
+    #         node = int(node)
+    #         cluster = int(cluster[:-1])
+    #         points_gurobi.loc[node, 'cluster_label'] = cluster
+    #         print(node, cluster)
+    #
+    # points_gurobi.plot.scatter(x='lon', y='lat', c=labels, s=50, cmap='viridis')
+    # plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+    # plt.show()
---
+++ optimization.py
@@ -0,0 +1,194 @@
+import gurobipy as gp
+from gurobipy import GRB
+import numpy as np
+import pulp
+
+
+def relocate_pulp(A, Q_c, q, points):
+    num_nodes = len(A)
+    num_clusters = len(A[0])
+
+    model = pulp.LpProblem("Model", pulp.LpMinimize)
+
+    # Variables
+    x_ic_add = pulp.LpVariable.dicts(
+            "x_ic_add_", ((node, cluster) for node in range(num_nodes)
+                          for cluster in range(num_clusters)),
+            cat=pulp.LpBinary
+            )
+    x_ic_rem = pulp.LpVariable.dicts(
+            "x_ic_rem_", ((node, cluster) for node in range(num_nodes)
+                          for cluster in range(num_clusters)),
+            cat=pulp.LpBinary
+            )
+
+    # Objective
+    model += pulp.lpSum(
+        [
+            [
+                (
+                    np.sqrt(
+                        (points.iloc[i]['lon'] - np.sum(
+                            [points.iloc[j]['lon'] * A[j, c]
+                             for j in range(num_nodes)]
+                             )) ** 2 +
+                        (points.iloc[i]['lat'] - np.sum(
+                            [points.iloc[j]['lat'] * A[j, c]
+                             for j in range(num_nodes)]
+                            )) ** 2
+                            )
+                    ) * (x_ic_add[i, c] - x_ic_rem[i, c]) for c in range(num_clusters)
+                ] for i in range(num_nodes)
+            ]
+        )
+
+    # Constraints
+    for node in range(num_nodes):
+        for cluster in range(num_clusters):
+            model += x_ic_rem[node, cluster] <= A[node, cluster]
+
+    for node in range(num_nodes):
+        model += pulp.lpSum([x_ic_rem[node, cluster] - x_ic_add[node, cluster]
+                             for cluster in range(num_clusters)]) == 0
+
+    for node in range(num_nodes):
+        for cluster_prime in range(num_clusters):
+            if A[node, cluster_prime] == 1:
+                model += pulp.lpSum(
+                        [x_ic_add[node, cluster]
+                         for cluster in range(num_clusters)
+                         if cluster != cluster_prime]
+                ) == x_ic_rem[node, cluster_prime]
+
+    for cluster in range(num_clusters):
+        model += pulp.lpSum(
+                [x_ic_add[node, cluster] for node in range(num_nodes)]
+                ) <= Q_c[cluster]
+
+    for cluster in range(num_clusters):
+        model += pulp.lpSum(
+                [q[node] * (
+                    A[node, cluster] + x_ic_add[node, cluster] -
+                    x_ic_rem[node, cluster]
+                    ) for node in range(num_nodes)]
+                ) <= Q_c[cluster]
+
+    for cluster in range(num_clusters):
+        if sum([A[node, cluster]*q[node] for node in range(num_nodes)]) <= Q_c[cluster]:
+            model += pulp.lpSum(
+                    [x_ic_rem[node, cluster] for node in range(num_nodes)]
+                    ) == 0
+
+    model.solve()
+
+    return model
+
+
+def relocate(a_matrix, q_c_vector, q, points):
+    num_nodes = len(a_matrix)
+    num_clusters = len(a_matrix[0])
+    nodes = list(range(num_nodes))
+    clusters = list(range(num_clusters))
+
+    model = gp.Model("Model")
+
+    # Variables
+    x_ic_add = model.addVars(nodes, clusters, vtype=GRB.BINARY, name="x_ic_add")
+    x_ic_rem = model.addVars(nodes, clusters, vtype=GRB.BINARY, name="x_ic_rem")
+
+    # Objective
+    model.setObjective(
+        gp.quicksum(
+            gp.quicksum(
+                np.sqrt(
+                    (points.iloc[i]['lon'] - np.sum(
+                        [points.iloc[j]['lon'] * a_matrix[j, c] for j in range(num_nodes)]
+                    )) ** 2 +
+                    (points.iloc[i]['lat'] - np.sum(
+                        [points.iloc[j]['lat'] * a_matrix[j, c] for j in range(num_nodes)]
+                    )) ** 2
+                ) * (x_ic_add[i, c] - x_ic_rem[i, c])
+                for c in range(num_clusters)
+            ) for i in range(num_nodes)
+        ),
+        GRB.MINIMIZE
+    )
+
+    # model.setObjective(
+    #     gp.quicksum(
+    #         gp.quicksum(
+    #             np.sqrt(
+    #                 (points.iloc[i]['lon'] - np.sum(
+    #                     [points.iloc[j]['lon'] * a_matrix[j, c] for j in range(num_nodes)]
+    #                 )) ** 2 +
+    #                 (points.iloc[i]['lat'] - np.sum(
+    #                     [points.iloc[j]['lat'] * a_matrix[j, c] for j in range(num_nodes)]
+    #                 )) ** 2
+    #             ) * (x_ic_add[i, c] - x_ic_rem[i, c])
+    #             for c in range(num_clusters)
+    #         ) for i in range(num_nodes)
+    #     ),
+    #     GRB.MINIMIZE
+    # )
+
+    # Constraints
+    for node in range(num_nodes):
+        for cluster in range(num_clusters):
+            model.addConstr(x_ic_rem[node, cluster] <= a_matrix[node, cluster])
+
+    for node in range(num_nodes):
+        model.addConstr(0 == gp.quicksum(x_ic_rem[node, cluster] - x_ic_add[node, cluster]
+                                         for cluster in range(num_clusters)))
+
+    for node in range(num_nodes):
+        for cluster_prime in range(num_clusters):
+            if a_matrix[node, cluster_prime] == 1:
+                model.addConstr(gp.quicksum(
+                    x_ic_add[node, cluster] for cluster in range(num_clusters) if cluster != cluster_prime
+                ) == x_ic_rem[node, cluster_prime])
+
+    for cluster in range(num_clusters):
+        model.addConstr(gp.quicksum(x_ic_add[node, cluster] for node in range(num_nodes)) <= q_c_vector[cluster])
+
+    for cluster in range(num_clusters):
+        model.addConstr(gp.quicksum(
+            q[node] * (a_matrix[node, cluster] + x_ic_add[node, cluster] -
+                       x_ic_rem[node, cluster]) for node in range(num_nodes)
+        ) <= q_c_vector[cluster])
+
+    for cluster in range(num_clusters):
+        if np.sum([a_matrix[node, cluster] * q[node] for node in range(num_nodes)]) <= q_c_vector[cluster]:
+            model.addConstr(0 == gp.quicksum(x_ic_rem[node, cluster] for node in range(num_nodes)))
+
+    model.optimize()
+
+    return model
+
+
+def tsp(nodes, distances):
+    n = len(nodes)
+    model = gp.Model('tsp')
+
+    # Variables
+    x = model.addVars(nodes, nodes, vtype=GRB.BINARY, name='x')
+    l = model.addVars(nodes, nodes, vtype=GRB.CONTINUOUS, name='l', lb=0.0, ub=n*1.0)
+
+    # Objective
+    model.setObjective(gp.quicksum(distances[i, j] * x[i, j] for i in nodes for j in nodes), GRB.MINIMIZE)
+
+    # Constraints
+    for i in nodes:
+        model.addConstr(1 == gp.quicksum(x[i, j] for j in nodes))
+        model.addConstr(1 == gp.quicksum(x[j, i] for j in nodes))
+
+    for j in nodes:
+        if j != 0:
+            model.addConstr(1 == gp.quicksum(l[i, j] - l[j, i] for i in nodes))
+
+    for i in nodes:
+        for j in nodes:
+            model.addConstr(l[i, j] <= (n-1) * x[i, j])
+
+    model.optimize()
+
+    return model, x
